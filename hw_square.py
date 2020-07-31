30ebb31bc6fe5112bea625248d0a3e3597c17806
alexandrmotologa <al.motologa@gmail.com>
2020-08-01 02:06:51 +0300

    inceput

---
+++ hw_square.py
@@ -0,0 +1,13 @@
+# PROVIDE A FUNCTION
+def draw_square(size):
+    print(("* " * size + "\n") * size + "\n")
+
+
+
+
+
+
+# CONSUME A FUNCTION
+
+draw_square(int(input("Size 1st sq: ")))
+draw_square(int(input("Size 2st sq: ")))
