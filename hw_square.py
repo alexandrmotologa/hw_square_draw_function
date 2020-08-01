30618586fdd6c90cf6274ed40ec18daded093b3e
alexandrmotologa <al.motologa@gmail.com>
2020-08-01 22:18:40 +0300

    hw1 + hw2

--- hw_square.py
+++ hw_square.py
@@ -1,13 +1,17 @@
 # PROVIDE A FUNCTION
 def draw_square(size):
-    print(("* " * size + "\n") * size + "\n")
+    rows = 0
+    while rows < size:
+        print(" * "*size)
+        rows += 1
 
 
-
-
-
+def inputInt(message):
+    # incercat cu float, ca sa fie acceptate si cifrele zecimale,
+    # dupa convertite in int (int(input(message)))
+    my_nr = float(input(message))
+    return int(my_nr)
 
 # CONSUME A FUNCTION
-
-draw_square(int(input("Size 1st sq: ")))
-draw_square(int(input("Size 2st sq: ")))
+draw_square(inputInt("Size 1st sq: "))
+draw_square(inputInt("Size 2st sq: "))
